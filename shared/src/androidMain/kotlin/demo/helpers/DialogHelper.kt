6f062c3a06b94fd03acb7c581d57a193121b3d6f
anatolijs.petjko <anatolijs.petjko@kevin.eu>
2023-06-06 15:14:24 +0300

    Cleanup DialogHelper

--- shared/src/androidMain/kotlin/demo/helpers/DialogHelper.kt
+++ shared/src/androidMain/kotlin/demo/helpers/DialogHelper.kt
@@ -1,14 +1,11 @@
 package demo.helpers
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
 
 @Composable
 actual fun ShowDialog(
@@ -20,18 +17,8 @@ actual fun ShowDialog(
     onDismissed: () -> Unit
 ) {
     AlertDialog(
-        title = {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleMedium
-            )
-        },
-        text = {
-            Text(
-                text = message,
-                style = MaterialTheme.typography.bodyMedium
-            )
-        },
+        title = { Text(title, style = MaterialTheme.typography.titleMedium) },
+        text = { Text(message, style = MaterialTheme.typography.bodyMedium) },
         shape = MaterialTheme.shapes.medium,
         containerColor = MaterialTheme.colorScheme.background,
         onDismissRequest = onDismissed,
@@ -40,18 +27,13 @@ actual fun ShowDialog(
                 Text(
                     text = negativeText,
                     style = MaterialTheme.typography.titleMedium,
-                    color = MaterialTheme.colorScheme.primary,
-                    modifier = Modifier.padding(end = 12.dp)
+                    color = MaterialTheme.colorScheme.primary
                 )
             }
         },
         confirmButton = {
             TextButton(onClick = onConfirmed) {
-                Text(
-                    text = positiveText,
-                    style = MaterialTheme.typography.titleMedium,
-                    color = Color.Red
-                )
+                Text(positiveText, style = MaterialTheme.typography.titleMedium, color = Color.Red)
             }
         }
     )
